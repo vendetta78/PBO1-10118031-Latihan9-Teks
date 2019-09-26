798988ac3d11b7f1368855d26578e029cb402c87
vendetta78 <55823091+vendetta78@users.noreply.github.com>
2019-09-26 12:02:28 +0700

    Add files via upload

---
+++ PBO1-10118031-Latihan9-Teks/src/pbo1/pkg10118031/latihan9/teks/PBO110118031Latihan9Teks.java
@@ -0,0 +1,24 @@
+package pbo1.pkg10118031.latihan9.teks;
+
+/**
+ *
+ * @author Vendetta
+ * NAMA              : Rizki Aria Putra
+ * KELAS             : PBO1
+ * NIM               : 10118031
+ * Deskripsi Program : Program ini berisi program untuk 
+ *                     varibel bertipe data Char
+ */
+public class PBO110118031Latihan9Teks {
+
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+
+    }
+
+}
